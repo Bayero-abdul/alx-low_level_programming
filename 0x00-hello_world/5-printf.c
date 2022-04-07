b4dbb378c0a947d90a5b58e6aacd488d83b43697
Bayero-abdul <bayeroa65@gmail.com>
2022-04-07 14:44:31 +0000

    C program that prints a string followed by a new line using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: 0 if programs runs without errors
+*/
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+	
+	printf("%s\n", str);
+	return (0);
+}
