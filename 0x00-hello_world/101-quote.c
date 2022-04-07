2a15b6b99740798d04794127b9a86f754b1c013d
Bayero-abdul <bayeroa65@gmail.com>
2022-04-07 20:20:25 +0000

    C program that prints a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: 0 if the function runs without an error
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
+	return (0);
+}
