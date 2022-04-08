36305d66074bb42a23bb50a6cf62b0a2eb1a4b4f
Bayero-abdul <bayeroa65@gmail.com>
2022-04-08 21:09:51 +0000

    Program that prints single digit numbers of base10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints all single digit numbers of base 10
+* using ascii
+*
+* Return: 0 if there is no error
+*/
+int main(void)
+{
+	int num_start, num_end;
+
+	num_start = 48;
+	num_end = 57;
+	while (num_start <= num_end)
+	{
+		putchar(num_start);
+		num_start++;
+	}
+	putchar('\n');
+	return (0);
+}
