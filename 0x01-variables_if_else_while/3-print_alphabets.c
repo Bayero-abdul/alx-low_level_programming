23a9a86287173598b7c2f81f2670cbd5dfca237d
Bayero-abdul <bayeroa65@gmail.com>
2022-04-08 18:21:09 +0000

    Remove trailing whitespaces

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int start_lower, end_lower, start_upper, end_upper;
 
-	start_lower = 96; 
-	end_lower = 122; 
-	start_upper = 64; 
+	start_lower = 96;
+	end_lower = 122;
+	start_upper = 64;
 	end_upper = 90;
 	while ((++start_lower) <= end_lower)
 	{
