ff8136f471c8aea20e9f904ebe1dadd496bbef5c
Bayero-abdul <bayeroa65@gmail.com>
2022-04-08 18:56:04 +0000

    Replace || with &&

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,7 +13,7 @@ int start_lower, end_lower;
 	end_lower = 122;
 	while ((++start_lower) <= end_lower)
 	{
-		if (start_lower != 'q' || start_lower != 'e')
+		if (start_lower != 101 && start_lower != 113)
 			putchar(start_lower);
 	}
 	putchar('\n');
