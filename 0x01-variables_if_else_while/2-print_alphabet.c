c41acbd69b09adf6b28c257b6a8015f4058665e3
Bayero-abdul <bayeroa65@gmail.com>
2022-04-08 17:27:18 +0000

    C program that prints lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabets in lowercase
+*
+* Return: if it runs successfully
+*/
+int main(void)
+{
+	int start, end;
+	
+	start = 96;
+	end = 122;
+	while ((++start) <= end)
+	{
+		putchar(start);
+	}
+	return (0);
+}
