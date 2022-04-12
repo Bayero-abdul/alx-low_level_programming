53470f2d3c2c86e7afe1a63178836e37fe25b408
Bayero-abdul <bayeroa65@gmail.com>
2022-04-12 15:42:19 +0000

    Program that prints 'a-z' 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main"
+
+/**
+* print_alphabet_x10 - Prints 'a-z' 10 times
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 97;
+
+		while (j < 122)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
