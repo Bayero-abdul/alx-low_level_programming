04695e2a19edeccfd7cecc8f2cd5c9ee9da2db42
Bayero-abdul <bayeroa65@gmail.com>
2022-04-13 18:58:49 +0000

    Remove lines

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
@@ -13,7 +12,7 @@ int main(void)
 
 	i = 0;
 
-	/**
+	/*
 	* while (i < 10)
 	* {
 	*	putchar(i);
@@ -21,6 +20,5 @@ int main(void)
 	*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
