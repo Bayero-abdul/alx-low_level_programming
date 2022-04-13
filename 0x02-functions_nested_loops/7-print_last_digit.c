9d9066db1030c3a6ebcbffae0ce9c404e00bb272
Bayero-abdul <bayeroa65@gmail.com>
2022-04-13 04:55:39 +0000

    Edit code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,10 @@
 */
 int print_last_digit(int n)
 {
-	int last_digit;
-	printf("value of n from call --> %d\n", n);
-	`last_digit = n % 10;
-  printf("value of last digit using putchar --> ");
-	_putchar(last_digit);
-	printf("\n");
-	printf("value of last digit using printf --> %d\n", last_digit);
-
-	return (0);
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	_putchar(n % 10 + '0');
+	return (n % 10);
 }
