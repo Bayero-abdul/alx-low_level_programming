dcb7685f0cb4abbc3499ad318c58099b3c515c0f
Bayero-abdul <bayeroa65@gmail.com>
2022-04-14 21:02:12 +0000

    Function that prints a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_line - Prints a lines n long
+* @n: number of time '_' is going to be printed
+*/
+void print_line(int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
