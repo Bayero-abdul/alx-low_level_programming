1b50d7a6969cfb7dd53516290ef8a6c0e65bcba8
Bayero-abdul <bayeroa65@gmail.com>
2022-04-14 19:30:10 +0000

    Function that prints 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* print_most_numbers - prints all single digit numbers of base 10
+* using ascii except 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int num_start, num_end;
+
+	num_start = 48;
+	num_end = 57;
+	while (num_start <= num_end)
+	{
+		if (num_start != 50 && num_start != 52)
+			putchar(num_start);
+		num_start++;
+	}
+	putchar('\n');
+}
