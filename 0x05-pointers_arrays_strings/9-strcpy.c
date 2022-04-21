6b489ab633d6ae0c3f22f52d310874040acb194f
Bayero-abdul <bayeroa65@gmail.com>
2022-04-21 04:40:00 +0000

    Function that copies a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcpy - copies string
+* @dest: destination the string is copied to
+* @src: where the string is copied from
+* Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*src)
+	{
+		if (*src == '\0')
+			break;
+
+		*(dest + i) = *src++;
+		i++;
+	}
+
+	return (dest);
+}
