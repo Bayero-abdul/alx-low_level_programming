f82486111f61fb07e76d56f12654e247dceacf99
Bayero-abdul <bayeroa65@gmail.com>
2022-04-24 14:41:11 +0000

    Function that reverses an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* reverse_array - reverse an array
+* @a: pointer to the address of
+* first element in the array
+* @n: number of elements in an array
+*/
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < (n - 1 - i); j++)
+		{
+			if (a[j] < a[j + 1])
+				swap_int(&a[j], &a[j + 1]);
+		}
+	}
+}
+
+
+/**
+* swap_int - swap value of two integers
+* @a: first integer
+* @b: second integer
+*/
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}
