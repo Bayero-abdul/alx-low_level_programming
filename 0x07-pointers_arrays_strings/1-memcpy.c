c9dc256833c17f1ffcd2c58acb0407f7e0567f10
bayero-abdul <bayeroa65@gmail.com>
2022-04-26 23:40:19 +0100

    Debug code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n && (&src[i] != dest); i++)
+	for (i = 0; i < n; i++)
 	{
-		*dest = src[i];
-		dest++;
+		dest[i] = src[i];
 	}
 
-	return (dest);
+	return (dest)
 }
