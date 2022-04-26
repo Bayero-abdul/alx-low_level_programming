67aa966e26dbb42ed77f8bc30ede14c3827b9f59
bayero-abdul <bayeroa65@gmail.com>
2022-04-27 00:50:02 +0100

    Remove trailing space

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,22 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int index;
-	unsigned int bytes = 0;
+int index;
+unsigned int bytes = 0;
 
-	while (*s)
-	{	
-		for (index = 0; accept[index]; index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
-			else if (accept[index + 1] == '\0')
-				return (bytes);
-		}
-		s++;
-	}
-	return (bytes);
+while (*s)
+{
+for (index = 0; accept[index]; index++)
+{
+if (*s == accept[index])
+{
+bytes++;
+break;
+}
+else if (accept[index + 1] == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
 }
