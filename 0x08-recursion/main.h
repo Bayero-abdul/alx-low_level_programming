80c31e39eced2dc44a806493f4bdd9fdeeaa4955
bayero-abdul <bayeroa65@gmail.com>
2022-04-30 05:25:29 +0100

    Function that checks if a number is prime

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _subtrahend(int n, int i);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
