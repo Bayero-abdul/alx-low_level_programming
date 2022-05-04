1704e00030f230cb2ef6fc565fffa58bd897cc25
Bayero-abdul <bayeroa65@gmail.com>
2022-05-04 13:11:03 +0000

    Update code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,24 +8,24 @@ void print_number(int n)
 {
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
-		print_number(n);
+		_print_number(n);
 	}
 	else if (n > 0)
 	{
 		if (n / 10 == 0)
 		{
-			putchar((n % 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		else
 		{
 			print_number(n / 10);
-			putchar((n % 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 	}
 }
