5c0c2b17f518bd1fd4d52afc19883c694bb7f106
Bayero-abdul <bayeroa65@gmail.com>
2022-05-04 06:26:24 +0000

    Refactor code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,21 @@
 
 char *leet(char *s)
 {
-	int count = 0, i;
-	int lower_case[] = {97, 101, 111, 116, 108};
-	int upper_case[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+	int i, j;
+	int lower_case[] = {'a', 'e', 'o', 't', 'l'};
+	int upper_case[] = {'A', 'E', 'O', 'T', 'L'};
+	int numbers[] = {'4', '3', '0', '7', '1'};
 
-
-	while (*(s + count) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < 5; i++)
+		for (j = 0; j < 5; j++)
 		{
-			if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
+			if (s[i] == lower_case[j] || s[i] == upper_case[j])
 			{
-				*(s + count) = numbers[i];
+				s[i] = numbers[j];
 				break;
 			}
 		}
-		count++;
 	}
-
 	return (s);
 }
