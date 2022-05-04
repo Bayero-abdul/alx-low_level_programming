71d07c89ee46a068d0c7668ac01d88ea19c8055e
Bayero-abdul <bayeroa65@gmail.com>
2022-05-04 07:37:22 +0000

    Function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string using rot13
+* @s: pointer to the string
+* Return: pointer to the string
+*/
+char *rot13(char *s)
+{
+	int i, j;
+	int letters[] = {
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '\0'};
+
+	int Rot_13[] = {
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', '\0'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = Rot_13[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
