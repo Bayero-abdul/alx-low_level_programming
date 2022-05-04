34d9701cbd96f7f897ecaba37ac43c983c4f9541
Bayero-abdul <bayeroa65@gmail.com>
2022-05-04 13:51:41 +0000

    Consider input(numbers) greater type 'int'

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,18 @@
 */
 void print_number(int n)
 {
+	unsigned int x;
+
+	x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = n * -1;
 	}
-	if (n / 10)
+	if (x / 10)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((x % 10) + '0');
 }
