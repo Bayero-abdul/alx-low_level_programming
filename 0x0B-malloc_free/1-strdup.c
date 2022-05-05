6bbb2bc600457ffea6484eeb1e7a88c5bc0a4b2c
Bayero-abdul <bayeroa65@gmail.com>
2022-05-05 11:42:04 +0000

    unction returns a pointer to a new string which is

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+
+/**
+* _strdup - returns a pointer to a newly allocated space in memory,
+* which contains a copy of the string given as a parameter
+* @str: pointer to string
+* Return: pointer to a duplicate of string pointer str is
+* pointing to
+*/
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *s;
+	int str_len = _strlen(str);
+
+	s = malloc((sizeof(char) * str_len) + 1);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	while ((s[i] = str[i]) != '\0')
+		i++;
+
+	return (s);
+}
+
+/**
+* _strlen - computes a length of a string
+* @s: pointer to the string
+* Return: length of the string
+*/
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+		p++;
+
+	return (p - s);
+}
