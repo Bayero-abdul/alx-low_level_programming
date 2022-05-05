dce68344356eafd55850af7862698e4ddfc40eac
Bayero-abdul <bayeroa65@gmail.com>
2022-05-05 19:25:05 +0000

    Allocate space for pointer to integer 'height' times

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		p[i] = malloc(width * sizeof(int));
 
