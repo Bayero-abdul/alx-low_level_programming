e4de13ba293d51f8912b31a4050964d04e132c13
Bayero-abdul <bayeroa65@gmail.com>
2022-05-05 11:08:49 +0000

    Allocate right amount of memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s = malloc((sizeof(char) * size) + 1);
+	s = malloc((sizeof(char) * size));
 
 	if (s == NULL)
 	{
