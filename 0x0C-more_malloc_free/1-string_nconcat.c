e66085f766b29b73d8a0a9a2511240d991ae985e
Bayero-abdul <bayeroa65@gmail.com>
2022-05-09 13:18:37 +0000

    Debug code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,6 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (j = 0; j < n && s2[j] != '\0'; i++, j++)
 		p[i] = s2[j];
+	
+	for (; j < n; j++)
+		p[i] = '\0';
 
 	p[i] = '\0';
 
