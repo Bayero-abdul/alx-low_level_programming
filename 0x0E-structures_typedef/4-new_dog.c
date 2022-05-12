7af12aca954ca3828e486fef41974d823f952a8b
Bayero-abdul <bayeroa65@gmail.com>
2022-05-12 09:20:44 +0000

    Function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* new_dog - creates a new dog
+* @name: name to assign
+* @age: age to assign
+* @owner: owner to assign
+* Return: pointer to struct dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t d, *cpy;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+
+	cpy = &d;
+	return (cpy);
+}
