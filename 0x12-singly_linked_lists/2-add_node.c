f612498c8f46bf1482a54dc72428fb89ee9f8984
Bayero-abdul <bayeroa65@gmail.com>
2022-05-24 16:57:04 +0000

    Add 'head' parameter description

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,6 +5,7 @@
 
 /**
 * add_node - adds a new node at the beginning of a list_t list
+* @head: pointer to pointer pointing to a list_t
 * @str: pointer to a string
 * Return: the address of the new element, or NULL if it failed
 */
