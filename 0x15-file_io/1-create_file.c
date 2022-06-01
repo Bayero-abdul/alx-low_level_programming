c4d40244f6184aa09abf3eda7fbbde5d3a13fc2c
Bayero-abdul <bayeroa65@gmail.com>
2022-06-01 20:32:08 +0000

    Test code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		len = _strlen(text_content) + 1;
+		len = _strlen(text_content);
 		n = write(fd, text_content, len);
 		if (n == -1)
 			return (-1);
