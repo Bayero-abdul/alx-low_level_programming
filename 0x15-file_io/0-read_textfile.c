f03cce01491f84718b6a91b14c5034b7bfb7e817
Bayero-abdul <bayeroa65@gmail.com>
2022-06-01 16:53:17 +0000

    Use betty syle

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,6 +6,13 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+/**
+* read_textfile - reads a text file and prints it to the POSIX
+* standard output
+* @filename: pointer to filename
+* @letters: number of letters it should read and print
+* Return: the actual number of letters it could read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, n, m;
@@ -18,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(sizeof(*buf) * letters);
 	if (buf == NULL)
 		return (0);
-	
+
 	n = read(fd, buf, letters);
 	m = write(STDOUT_FILENO, buf, n);
 	if (n == -1 || n != m)
