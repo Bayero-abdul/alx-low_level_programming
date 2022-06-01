a4e79f1988418a8de7fa951d659320378a406880
Bayero-abdul <bayeroa65@gmail.com>
2022-06-01 20:13:09 +0000

    Function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+/**
+* create_file - creates a file
+* @filename: pointer to filename
+* @text_content: text to be written on the file after created
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, n, len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		len = _strlen(text_content) + 1;
+		n = write(fd, text_content, len);
+		if (n == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+/**
+* _strlen - Returns the length of a string
+* @s: pointer to character
+* Return: length of the string 'count'
+*/
+int _strlen(char *s)
+{
+		int count;
+
+		for (count = 0; *s != '\0'; s++)
+			count++;
+
+		return (count);
+}
