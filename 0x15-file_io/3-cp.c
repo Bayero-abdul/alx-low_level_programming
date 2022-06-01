b7cef1af759101f5be6224972db851b2a0bd3eb0
Bayero-abdul <bayeroa65@gmail.com>
2022-06-01 23:06:21 +0000

    Program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+/**
+* main - copies content from one file to another
+* @argc: number of command line argument
+* @argv: array of strings
+* Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+	ssize_t w, r, n;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	r = open(argv[1], O_RDONLY);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	w = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 00664);
+	if (w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((n = read(r, buf, 1024)) > 0)
+	{
+		write(w, buf, 1024);
+	}
+
+	if (close(w) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", w);
+		exit(100);
+	}
+	if (close(r) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", r);
+		exit(100);
+	}
+
+	return (0);
+}
