6b2c9e2feb3757ea962f5161f693ecf96989fc91
Bayero-abdul <bayeroa65@gmail.com>
2022-06-04 17:33:00 +0000

    Update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stddef.h>
 #include <sys/types.h>
 
@@ -9,5 +8,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
+void close_file(int n, ssize_t *m);
 
 #endif
