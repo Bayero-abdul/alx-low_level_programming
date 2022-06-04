735f9346b08c79af6b88d40e64a086128b71b6c8
Bayero-abdul <bayeroa65@gmail.com>
2022-06-04 17:05:34 +0000

    Test code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@
 int main(int argc, char **argv)
 {
 	ssize_t w, r, n;
-	char buf[1024];
+	char buf[1025];
 
 	if (argc != 3)
 	{
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	w = creat(argv[2], 0664);
+	w = creat(argv[2], 00664);
 	if (w == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -38,19 +38,25 @@ int main(int argc, char **argv)
 	}
 
 	while ((n = read(r, buf, 1024)) > 0)
-		write(w, buf, n);
-
+	{
+		if (write(w, buf, n) == n)
+			continue;
+	}
+		
 	buf[n] = '\0';
 
-	if (close(w) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", w);
-		exit(100);
-	}
-	if (close(r) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", r);
-		exit(100);
-	}
+	close_file(close(w), &w);
+	close_file(close(r), &r);
+
 	return (0);
 }
+
+void close_file(int n, ssize_t *m)
+{
+	 if (n == -1)
+	 {
+		 dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", *m);
+		 exit(100);
+	 }
+}
+
