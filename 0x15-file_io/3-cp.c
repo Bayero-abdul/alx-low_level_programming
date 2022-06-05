6d706736928e4058ad809693ec898f36ce048cf5
Bayero-abdul <bayeroa65@gmail.com>
2022-06-05 22:29:56 +0000

    Check for read() and write() return error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,14 +39,20 @@ int main(int argc, char **argv)
 
 	while ((n = read(r, buf, 1024)) > 0)
 	{
+		if (n == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		buf[n] = '\0';
 		if (write(w, buf, n) != n)
-			break;
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-
 	close_file(close(w), &w);
 	close_file(close(r), &r);
-
 	return (0);
 }
 
