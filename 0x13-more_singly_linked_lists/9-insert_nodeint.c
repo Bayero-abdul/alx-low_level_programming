89800559c34bb3b15e733aa56fab79abd7cb7af4
Bayero-abdul <bayeroa65@gmail.com>
2022-07-03 16:15:22 +0000

    Check if malloc return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 
