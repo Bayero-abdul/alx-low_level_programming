f131a722e2f9fa8c96aae499e88207087d57dc8d
Bayero-abdul <bayeroa65@gmail.com>
2022-09-01 14:05:35 +0000

    Debug code

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		temp = *head;
 		*head = (*head)->next;
-		if (*head != NULL && (*head)->next != NULL)
+		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(temp);
 		return (1);
