a9b8bbe84d8783e5b5c0c16ac7aa7c9f74ec8b23
Bayero-abdul <bayeroa65@gmail.com>
2022-09-01 16:47:23 +0000

    Debug code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,17 +12,16 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head)
-	{
-		i = 0;
-		while (head)
-		{
-			if (i == index)
-				return (head->n);
-			head = head->next;
-			i++;
-		}
-
+	if (!head)
 		return (NULL);
+
+	i = 0;
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
 	}
+	return (NULL);
 }
