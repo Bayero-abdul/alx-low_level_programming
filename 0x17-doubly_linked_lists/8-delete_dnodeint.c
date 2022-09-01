ed8ce5d8d3f3a221911487db0edd40e6abc25d73
Bayero-abdul <bayeroa65@gmail.com>
2022-09-01 14:02:40 +0000

    Point pointer prev to NULL if node is more than one

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,6 +21,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		temp = *head;
 		*head = (*head)->next;
+		if (*head != NULL && (*head)->next != NULL)
+			(*head)->prev = NULL;
 		free(temp);
 		return (1);
 	}
