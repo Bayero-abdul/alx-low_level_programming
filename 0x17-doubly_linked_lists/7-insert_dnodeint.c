795a1f5e9aac3fc2eac40809fa74380eabf526ce
Bayero-abdul <bayeroa65@gmail.com>
2022-09-02 20:25:09 +0000

    Debug code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,38 +12,58 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current, *new_node;
-	unsigned int i;
+	unsigned int i, num_of_nodes;
+
+	num_of_nodes = dlistint_len(*h);
+	if (idx + 1 > num_of_nodes)
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	if (idx == 0)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
 		*h = new_node;
-		return (new_node);
 	}
 	else
 	{
-		current = *h;
 		i = 0;
-		while (current->next && i < idx - 1)
-		{
+		current = *h;
+		while (i++ < idx - 1)
 			current = current->next;
-			i++;
-		}
-		
-		if (idx != i + 1 && current->next == NULL)
-			return (NULL);
 
-		new_node->next = current->next;
 		new_node->prev = current;
+		new_node->next = current->next;
+		new_node->next->prev = new_node;
 		current->next = new_node;
 	}
-
 	return (new_node);
+}
+
+
+/**
+ * dlistint_len - returns the number of elements in a linked
+ * @h: pointer to a double linked list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int num_of_nodes = 0;
+
+	if (!h)
+		return (num_of_nodes);
+
+	while (h)
+	{
+		num_of_nodes++;
+		h = h->next;
+	}
+	return (num_of_nodes);
 }
