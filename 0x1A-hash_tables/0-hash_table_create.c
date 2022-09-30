9d40d96f08a605e460cc1095b2e4afae750f60bc
Bayero-abdul <bayeroa65@gmail.com>
2022-09-30 16:51:45 +0000

    Debug and Refactor code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(*ht));
+	unsigned long i;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
@@ -18,5 +19,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < ht->size; i++)
+		ht->array[i] = NULL;
+
 	return (ht);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	walk = ht->array[index];
+	/* change the value if key exist */
+	while (walk != NULL)
+	{
+		if (strcmp(walk->key, key) == 0)
+		{
+			walk->value = strcpy(walk->value, value);
+			return (1);
+		}
+		walk = walk->next;
+	}
+
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return (0);
@@ -25,26 +38,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
-	index = key_index((const unsigned char *)key, ht->size);
-
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
 	}
 	else
 	{
-		walk = ht->array[index];
-		while (walk != NULL)
-		{
-			if (strcmp(walk->key, key) == 0)
-			{
-				free(walk->value);
-				walk->value = strdup(value);
-				return (1);
-			}
-			walk = walk->next;
-		}
-
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
