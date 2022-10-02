d8bb75bed77bb759c2bac33e58741d54e5b4a6e8
Bayero-abdul <bayeroa65@gmail.com>
2022-10-02 12:33:53 +0000

    Refactor code

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,16 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	walk = *head;
-	/* if list contains one element */
-	if (index == 0 && (*head)->next == NULL)
-	{
-		free(*head);
-		*head = NULL;
-		return (1);
-	}
-
-	if (index == 0 && (*head)->next != NULL)
+	if (index == 0)
 	{
 		temp = *head;
 		*head = (*head)->next;
