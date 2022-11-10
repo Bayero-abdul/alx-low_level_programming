6e5614c77cf334d8430d150c6d14bcb9ee71ea3c
Bayero-abdul <bayeroa65@gmail.com>
2022-11-10 16:40:42 +0000

    Make code efficient

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-int is_prime(unsigned long n);
-
 /**
 * main - prints the largest prime factor of
 * the number 612852475143
@@ -9,42 +7,19 @@ int is_prime(unsigned long n);
 */
 int main(void)
 {
-	unsigned long i = 2;
+	unsigned long i;
 	unsigned long num =  612852475143;
 	unsigned long largest_prime;
 
-	while (i <= num)
+	for (i = 2; i * i <= num; i++)
 	{
-		if (num % i == 0 && is_prime(i))
+		while (num % i == 0)
 		{
-			num /= i;
 			largest_prime = i;
-		}
-		else
-		{
-			i++;
+			num /= i;
 		}
 	}
 	printf("%ld\n", largest_prime);
 
 	return (0);
-}
-
-/**
-* is_prime - Check if number is prime
-* @n: number
-* Return: 1 if number is prime else
-* return 0
-*/
-int is_prime(unsigned long n)
-{
-	unsigned long i = 2;
-
-	while (i < n)
-	{
-		if (n % i == 0)
-			return (0);
-		i++;
-	}
-	return (1);
 }
