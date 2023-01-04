23b30163fd8082519c559dff2e9da39849df5da6
Bayero-abdul <bayeroa65@gmail.com>
2023-01-04 17:01:36 +0000

    Debug code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -41,5 +41,7 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", low, array[low]);
 		return (low);
 	}
+
+	printf("Value checked array[%ld] = [%d]\n", low, array[low]);
 	return (-1);
 }
