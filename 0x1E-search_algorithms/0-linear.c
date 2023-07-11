4ef0ce68e2cb95aa9fe6f5d49c6a9280d1a746bc
Bayero-abdul <bayeroa65@gmail.com>
2023-07-11 18:12:11 +0100

    Update task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,10 +13,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL && *array == NULL)
+	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
